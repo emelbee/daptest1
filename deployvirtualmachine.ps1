d83e9bd09b9e52983a22ca791e572ce06f7d1940
emelbee <35521179+emelbee@users.noreply.github.com>
2020-10-29 15:13:24 +0100

    Create deployvirtualmachine.ps1

---
+++ deployvirtualmachine.ps1
@@ -0,0 +1,13 @@
+New-AzResourceGroup `
+   -ResourceGroupName "myResourceGroupVM" `
+   -Location "EastUS"
+   
+   New-AzVm `
+    -ResourceGroupName "myResourceGroupVM" `
+    -Name "myVM" `
+    -Location "EastUS" `
+    -VirtualNetworkName "myVnet" `
+    -SubnetName "mySubnet" `
+    -SecurityGroupName "myNetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -Credential admin admin
