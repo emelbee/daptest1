a3c6c08907d05d4729bad9c979f6a00c4ebe6ad5
emelbee <35521179+emelbee@users.noreply.github.com>
2020-10-30 17:11:11 +0100

    Update onboardasset.ps1

--- onboardasset.ps1
+++ onboardasset.ps1
@@ -10,8 +10,8 @@ $logonInfo = @{}
 
   #$logonInfo.username = $username
   #$logonInfo.password = $password
-  $logonInfo.username = $(vault-username)
-  $logonInfo.password = $(vault-password)
+  $logonInfo.username = $(env:vault-username)
+  $logonInfo.password = $(env:vault-password)
 
 # test input
 $myPublicIpAddress = "1.1.1.1"
