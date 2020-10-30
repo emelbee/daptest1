abb71cac2ad0395981699617609c33e75ec0f8d7
emelbee <35521179+emelbee@users.noreply.github.com>
2020-10-30 13:41:26 +0100

    Update deployvirtualmachine.ps1

--- deployvirtualmachine.ps1
+++ deployvirtualmachine.ps1
@@ -21,15 +21,14 @@ New-AzResourceGroup `
    #-Force
 
 
-#New-AzVm `
-#    -ResourceGroupName "myResourceGroupVM" `
-#    -Name "myVM" `
-#    -Location "East US" `
-#    -VirtualNetworkName "myVnet" `
-#    -SubnetName "mySubnet" `
-#    -SecurityGroupName "myNetworkSecurityGroup" `
-#    -PublicIpAddressName "myPublicIpAddress" `
-#    -OpenPorts 80,3389 `
-#    -Credential $Cred `
-#    -Force
+New-AzVm `
+    -ResourceGroupName "myResourceGroupVM" `
+    -Name "myVM" `
+    -Location "East US" `
+    -VirtualNetworkName "myVnet" `
+    -SubnetName "mySubnet" `
+    -SecurityGroupName "myNetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -OpenPorts 80,3389 `
+    -Credential $Cred 
 
