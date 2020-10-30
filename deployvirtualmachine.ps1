312bd9b9adbb748740e386eb25098fceb5a31f50
emelbee <35521179+emelbee@users.noreply.github.com>
2020-10-30 11:56:19 +0100

    Update deployvirtualmachine.ps1

--- deployvirtualmachine.ps1
+++ deployvirtualmachine.ps1
@@ -1,11 +1,11 @@
 New-AzResourceGroup `
-   -ResourceGroupName "myResourceGroupVM" `
+   -ResourceGroupName "myResourceGroupVM1" `
    -Location "East US"
    -Force
    
   
-  $username = "newuser"
-  $password = ConvertTo-SecureString "mypassword" -AsPlainText -Force
+ # $username = "newuser"
+ # $password = ConvertTo-SecureString "mypassword" -AsPlainText -Force
  # $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password) -Force
       
  #New-AzVm `
@@ -20,14 +20,14 @@ New-AzResourceGroup `
    #-Force
 
 
-New-AzVm `
-    -ResourceGroupName "myResourceGroupVM" `
-    -Name "myVM" `
-    -Location "East US" `
-    -VirtualNetworkName "myVnet" `
-    -SubnetName "mySubnet" `
-    -SecurityGroupName "myNetworkSecurityGroup" `
-    -PublicIpAddressName "myPublicIpAddress" `
-    -OpenPorts 80,3389 `
-    -Credential $Cred `
-    -Force
+#New-AzVm `
+#    -ResourceGroupName "myResourceGroupVM" `
+#    -Name "myVM" `
+#    -Location "East US" `
+#    -VirtualNetworkName "myVnet" `
+#    -SubnetName "mySubnet" `
+#    -SecurityGroupName "myNetworkSecurityGroup" `
+#    -PublicIpAddressName "myPublicIpAddress" `
+#    -OpenPorts 80,3389 `
+#    -Credential $Cred `
+#    -Force
