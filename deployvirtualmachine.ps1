d7923d4407af6b90c4d2c558aff1661527256415
emelbee <35521179+emelbee@users.noreply.github.com>
2020-10-30 11:27:49 +0100

    Update deployvirtualmachine.ps1
    backticks added

--- deployvirtualmachine.ps1
+++ deployvirtualmachine.ps1
@@ -1,21 +1,18 @@
-New-AzResourceGroup 
-   -ResourceGroupName "myResourceGroupVM" 
+New-AzResourceGroup `
+   -ResourceGroupName "myResourceGroupVM" `
    -Location "East US"
    
-  #$password = ConvertTo-SecureString “TestTestTest12345!” -AsPlainText -Force
-  #$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList (“forceduser” , $password)
-  
   
   $username = "newuser"
   $password = ConvertTo-SecureString "mypassword" -AsPlainText -Force
   $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password) -Force
       
- New-AzVm 
-   -ResourceGroupName "myResourceGroupVM" 
-   -Name "myVM" 
-   -Location "East US" 
-   -VirtualNetworkName "myVnet" 
-   -SubnetName "mySubnet" 
-   -SecurityGroupName "myNetworkSecurityGroup" 
-   -PublicIpAddressName "myPublicIpAddress" 
+ New-AzVm `
+   -ResourceGroupName "myResourceGroupVM" `
+   -Name "myVM" `
+   -Location "East US" `
+   -VirtualNetworkName "myVnet" `
+   -SubnetName "mySubnet" `
+   -SecurityGroupName "myNetworkSecurityGroup" `
+   -PublicIpAddressName "myPublicIpAddress" `
    -Credential $Cred
